5d981e34166df7fe83ca45a0183dd7c7b0f55896
OldGregg52 <gjb52@zips.uakron.edu>
2020-10-11 23:19:12 -0400

    Changes

--- Final/final.cpp
+++ Final/final.cpp
@@ -6,107 +6,132 @@
  * Purpose: To produce a minimalist shell via the use of execvp() and fork() functions
  */
 
-#include <iostream>
-#include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <sys/wait.h>
+#include <iostream>     //Included for C++ standard input and output
+#include <stdio.h>      //Included for C standard input and output
+#include <string.h>     //Included for functions dealing with strings, such as strcpy()
+#include <unistd.h>     //Included for various Unix functions such as fork() and execvp()
+#include <stdlib.h>     //Included for C standard functions used in this program
+#include <sys/wait.h>   //Included for the wait() function
 
+//Defining of necessary global constants for dynamic variable allocation
+//      and variable initialization
 #define MAX_ARGS        64
 #define MAX_ARG_LEN     16
 #define MAX_LINE_LEN    80
 #define WHITESPACE      " ,\t\n"
 
+//Declaring the struct variable 'command_t' that will be used to hold the
+//      values entered by the user and the values that will be passed to
+//      the execvp() function for process calling
 struct command_t
 {
-    char *name;
-    int argc;
-    char *argv[MAX_ARGS];
-    char *args[4];
+    char *name;             //Char pointer used for the string needed for the command calls
+    int argc;               //Integer variable used to determine how many entries the user input
+    char *argv[MAX_ARGS];   //Char pointer array to hold all the words/symbols the user inputs
+    char *args[4];          //Char pointer array to hold the arguments that will be passed to execvp()
 };
 
+//Function descriptions and purposes are labeled after the main() function
 int parseCommand(char*, struct command_t*);
 void readCommand(char*);
 void printPrompt();
-int callFork(struct command_t*, int*, int&, int&, int&);
+int callFork(struct command_t*, int*, int&);
 void helpPrint();
 bool setStructValues(struct command_t*);
 void errorMessage(struct command_t*);
 
 int main()
 {
-    int pid, status, foxPid, chPid;
+    int pid, status;
     char cmdLine[MAX_LINE_LEN];
     struct command_t command;
 
-    while(true)
+    command.args[0] = (char *) malloc(MAX_ARG_LEN);    //Array of character pointers initialized to be
+    command.args[1] = (char *) malloc(MAX_ARG_LEN);    //     able to hold the MAX_ARG_LEN amount of characters
+    command.args[2] = (char *) malloc(MAX_ARG_LEN);    //     so the arguments needed for the execvp() function
+    command.args[3] = (char *) malloc(MAX_ARG_LEN);    //     can be assigned to them
+
+    while(true)     //While-loop initialized to constantly run for continuous command execution
     {
-        printPrompt();
-        readCommand(cmdLine);
-        parseCommand(cmdLine, &command);
+        printPrompt();      //Function prints the prompt to the screen so the user knwos to enter input
+        readCommand(cmdLine);   //Function reads what the user inputs and stores it in a character array
+        parseCommand(cmdLine, &command);    //Function parses the inputted array so words are stored in individual variables
 
-        command.argv[command.argc] = NULL;
+        command.argv[command.argc] = NULL;    //The end of the character array used for parsing is set to NULL
 
-        switch(*command.name)
-        {
+        switch(*command.name)       //The 'name' member variable of the 'command' struct is used to
+        {                           //    determine what action will be taken
         case 'E':
-            {
-                printPrompt();
-                int i = 1;
-                while(command.argv[i] != NULL)
+            {                                     //If 'E' is input then the prompt is printed to the screen again
+                printPrompt();                    //     and a while-loop is established to iterate through the
+                int i = 1;                        //     'argv' character pointer array and print all of the contents to
+                while(command.argv[i] != NULL)    //     the terminal how they were entered, with only spaces between
                 {
                     std::cout << command.argv[i] << " ";
                     i++;
                 }
-                std::cout << std::endl;
+                std::cout << std::endl;           //New line is set and the switch-statement breaks
             }
             break;
         case 'H':
-            helpPrint();
-            break;
+            helpPrint();     //If 'H' is input then the helpPrint() function is called to display the
+            break;           //     correct information, then the switch-statement breaks
         case 'L':
-            setStructValues(&command);
-            callFork(&command, &pid, status, foxPid, chPid);
-            wait(&status);
-            std::cout << std::endl;
-            strcpy(command.name, "ls");
-            strcpy(command.args[0], command.name);
-            strcpy(command.args[1], "-l");
+            setStructValues(&command);        //If 'L' is input then the setStructValues() function is called to
+            callFork(&command, &pid, status); //     set the "name" and "args" values before calling the callFork()
+            wait(&status);                    //     function to implement the correct process
+            std::cout << std::endl;           //A new line is established and waits until the callFork() function finishes
+            strcpy(command.name, "ls");              //After the path is displayed, the "name" and "args" values are
+            strcpy(command.args[0], command.name);   //     changed so the callFork() function can be called again
+            strcpy(command.args[1], "-l");           //     to display a list of files and folders in the current directory
             command.args[2] = NULL;
-            callFork(&command, &pid, status, foxPid, chPid);
+            callFork(&command, &pid, status);
             wait(&status);
-            std::cout << std::endl;
-            break;
+            std::cout << std::endl;     //A new line is established and waits until the callFork() function finishes
+            break;                      //     then the switch-statement breaks
         case 'Q':
             std::cout << "\n\tshell: Terminating successfully\n";
-            return(0);
+            return(0);                  //If 'Q' is input, then a message is displayed and main() function terminates
             break;
         default:
-            if(*command.name != '\0')
-            {
-                if(!setStructValues(&command))
+            if(*command.name != '\0')           //If none of the above options are chosen then the setStructValues()
+            {                                   //     function is called, which returns a bool value depending on if
+                if(!setStructValues(&command))  //     chosen input doesn't match any of the commands
                 {
-                    callFork(&command, &pid, status, foxPid, chPid);
-
-                    if(*command.name != 'f')
-                        wait(&status);
+                    callFork(&command, &pid, status);     //If there is a matching command then the "name" and "args"
+                                                          //     values are set and the callFork() function is called
+                    if(*command.name != 'f')     //If the called function is not firefox, then the wait() function
+                        wait(&status);           //     is used to ensure commands are performed sequentially
                 }
-                else
-                    errorMessage(&command);
-            }
-            break;
+                else                             //If there isn't a matching command to the input, then the
+                    errorMessage(&command);      //     errorMessage() function is called to display the correct
+            }                                    //     information. Then the switch-statement breaks and the
+            break;                               //     indefinite while-loop iterates
         }
     }
 
     return(0);
 }
 
+/*
+ * readCommand() function takes a character pointer as an input and reads in
+ *     80 characters from the standard terminal input. Nothing is returned
+ *     but the pointer "buffer" is set to the position of the input string
+ */
 void readCommand(char* buffer)
 {
     fgets(buffer, 80, stdin);
 }
 
+/*
+ * parseCommand() function takes a character pointer and a struct variable pointer as input
+ *     the character pointer is the same one the readCommand() function uses. The string that
+ *     is pointed to is then broken down into individual words that are then individually
+ *     assigned to a pointer in the "argv" member variable array. In addition, the variable
+ *     "argc" is updated to hold the number of words that were inputted into the terminal.
+ *     Finally, the "name" member variable is updated to the value that the initial "argv"
+ *     pointer is pointing to. An integer '1' is returned when the function is complete.
+ */
 int parseCommand(char* cLine, struct command_t* cmd)
 {
     int argc;
@@ -127,37 +152,38 @@ int parseCommand(char* cLine, struct command_t* cmd)
     return 1;
 }
 
-
-int callFork(struct command_t* cmd, int* pid, int &status, int &foxPid, int &chPid)
+/*
+ * callFork() function takes a struct and integer pointers as input, as well as an
+ *     integer variable that was passed by reference. It then determines if a new
+ *     function can be initiated. If it can, then the member variables of the struct
+ *     "args" and "name" are used to pass values to the execvp() function to start
+ *     the process. It also include the perror() function to prevent abnormal behavior if
+ *     the value in "name" is not recognized.
+ */
+int callFork(struct command_t* cmd, int* pid, int &status)
 {
     if((*pid = fork()) == 0)
     {
         execvp(cmd->name, cmd->args);
         perror(cmd->name); return -1;
-        foxPid = *pid;
     }
-    /*else
-    {
-        wait(&status);
-        if(foxPid > 0)
-        {
-            chPid = waitpid(foxPid, &status, WNOHANG);
-            if(chPid == foxPid)
-            {
-                std::cout << "Detecting firefox termination.\n";
-                kill(foxPid, SIGTERM); foxPid = 0;
-            }
-        }
-    }*/
-
     return 0;
 }
 
+/*
+ * printPrompt() function is used to print the input prompt to the console whenever
+ *     it is called.
+ */
 void printPrompt()
 {
     std::cout << "linux gjb52 |> ";
 }
 
+/*
+ * helpPrint() function takes no input and only prints text to the screen to indicate how
+ *     to properly use the terminal, along with a list of available commands and which of
+ *     those commands require file paths in addition to the command itself.
+ */
 void helpPrint()
 {
      std::cout << std::endl << "To use this console, please enter the command you wish to execute with a" << std::endl
@@ -184,14 +210,20 @@ void helpPrint()
      return;
 }
 
+/*
+ * setStructValues() function takes a struct pointer as a variable and uses it to reset the "args"
+ *     values before deteremining what values must be assigned to those pointers based on what input
+ *     was detected via switch-statement. For the commands that require arguments, the strcpy()
+ *     function is utilized to transfer the string that is held in the corresponding "argv" variable
+ *     to the necessary "args" variable. If the command doens't require input, the "args" values are
+ *     set to either '\0' or NULL, depending on what the corresponding command requires. In addition to
+ *     this, if input is detected that doesn't match any of the switch-statement cases, then a bool
+ *     value is assigned to the variable "errorBool" and returned to show that the errorMessage()
+ *     function should be called.
+ */
 bool setStructValues(struct command_t* cmd)
 {
     bool errorBool = false;
-
-    cmd->args[0] = (char *) malloc(MAX_ARG_LEN);
-    cmd->args[1] = (char *) malloc(MAX_ARG_LEN);
-    cmd->args[2] = (char *) malloc(MAX_ARG_LEN);
-    cmd->args[3] = (char *) malloc(MAX_ARG_LEN);
 
     cmd->args[0] = cmd->name;
     *cmd->args[1] = *cmd->args[2] = '\0';
@@ -247,18 +279,24 @@ bool setStructValues(struct command_t* cmd)
         errorBool = true;
         break;
     }
-
     return errorBool;
 }
 
+/*
+ * errorMessage() function takes a struct pointer as input and prints to the screen that there
+ *     has been an error. From there, it determines if the input was just a command that was
+ *     lowercase instead of uppercase, or if was an entirely different input altogether. If
+ *     the input is just lowercase, then the switch-statement is directed to print a message
+ *     telling the user of the input they possibly meant instead. Otherwise, the message is
+ *     sent that this function doesn't know what the user wanted and directs the user to
+ *     the help guide for more information.
+ */
 void errorMessage(struct command_t* cmd)
 {
     std::cout << "\n\t!--The entered command doesn't match any preexisitng commands--!\n\n";
     std::cout << "\tCommand Entered: " << *cmd->name << std::endl << std::endl << '\t';
 
-    char temp = *cmd->name;
-
-    switch(temp)
+    switch(*cmd->name)
     {
     case 'c':
         std::cout << "Closest Possible Command: C, for 'COPY'";
